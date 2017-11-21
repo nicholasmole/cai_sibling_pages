d63fde26d5c53d0c72827f2421e0be932e8d436a
Nick M <onethousandmarkers@gmail.com>
2017-11-21 09:26:51 -0500

    edited spw - on helper page

--- src/classes/Helpers.php
+++ src/classes/Helpers.php
@@ -21,9 +21,9 @@ class Helpers {
   public function check_if_parent_page($page) {
     //check if parent || else if active level 
     if((wp_get_post_parent_id( $post_ID ) == $page->ID) || ((get_the_ID() == $page->ID) && (wp_get_post_parent_id( $post_ID ) == 0 ) ) ) {
-      return ' mole-sidebar-parent ';
+      return ' spw-sidebar-parent ';
     }else if(get_the_ID() == $page->ID){
-      return ' mole-sidebar-active ' ;
+      return ' spw-sidebar-active ' ;
     }
   }
 
