7bd206e2668713b90f0b8fa21b0d84e3fc6c8f2d
Nick M <onethousandmarkers@gmail.com>
2017-12-11 21:52:30 -0500

    Added code for CaiSofts required needs
    Able to collect the children pages when needed
    and hide the grandchildren unless the direct
    parent is selected.
    
    added fix for news_feed_page
    News feed page wouldn't direct to proper page

--- src/classes/Helpers.php
+++ src/classes/Helpers.php
@@ -4,26 +4,88 @@ namespace JB\SPW;
 
 class Helpers {
 
+  private static $currentID = 0;
+
+  public function set_currentID($value){
+
+    self::$currentID = $value;
+  }
+  public function get_currentID(){
+
+    return self::$currentID;
+  }
+
   public static function get_template_path($template_filename) {
     $templates_path = plugin_dir_path(dirname(__FILE__)) . "templates";
     $template_path = "$templates_path/$template_filename";
 
     return $template_path;
-  }
+  } 
 
   public static function build_sidebar_link($post) {
     $name = $post->post_title;
+    //Check news
+    if($name=="Ponderosa News" ||$name=="ProVisions News"||$name=="JMAS News" ||$name=="Seasoft News"  ):
+       $name = "News";
+    endif;
     $url = get_permalink($post->ID);
 
     return "<a href=\"$url\">$name</a>";
   }
 
+  //Gets number of Ancestors for post
+  //Used for Tracking Second Tier Parent at Any Tier
+  //Offset used when value is used in array ie. get_id_of_second_tier_parent
+  public function get_num_of_parents($current_post, $offset){
+    return sizeof( get_post_ancestors( $current_post ) ) - $offset;
+  }
+
+  //Gets ID of Parent 
+  //Figures out Parent at level based on tier level of current_post
+  public function get_id_of_second_tier_parent($current_post,$offset){
+    return get_post_ancestors( $current_post )[self::get_num_of_parents($current_post,$offset)];
+  }
+
+  
   public function check_if_parent_page($page) {
     //check if parent || else if active level 
-    if((wp_get_post_parent_id( $post_ID ) == $page->ID) || ((get_the_ID() == $page->ID) && (wp_get_post_parent_id( $post_ID ) == 0 ) ) ) {
-      return ' spw-sidebar-parent ';
-    }else if(get_the_ID() == $page->ID){
+    if(self::get_id_of_second_tier_parent($page ,1 ) == $page->ID ){
+      return ' spw-sidebar-parent ' ;
+    }else if(self::get_currentID() == $page->ID){
       return ' spw-sidebar-active ' ;
+    }
+  }
+
+  public function check_if_parent_is_active($page) {
+
+    //If passed post == ID of current page
+    if( ($page->ID == self::get_currentID()) ){
+      //if this is the 5th Tier Child or 4th tier parent
+      if((self::get_num_of_parents($page, 1) > 1)){
+        return 'spw-child i-am-this-page ';
+      }else{
+        return ' i-am-this-page ';
+      }
+    //If this is the 5th Child && it shouldn't be there because parent isn;t active 
+    }else if ((self::get_num_of_parents($page, 1) > 1) && !(wp_get_post_parent_id($page->ID) == self::get_currentID()) ){
+      return ' spw-display-remove '; 
+    //if this is the 5th Tier Child Sibling Identified by the 4th Tier Parent
+    }else if( wp_get_post_parent_id($page->ID) == self::get_currentID()  ){
+      
+      if((self::get_num_of_parents($page, 1) > 1)){
+        return 'spw-child i-am-this-child ';
+      }else{
+        return ' i-am-this-child ';
+      }
+    //This passed Post is The parent of the current page
+    }else if( ($page->ID == self::get_id_of_second_tier_parent(self::get_currentID() , 2 )) ){
+      return ' my-parent-page ';
+    //This parent of the passed post is at the level of 2nd Tier 
+    }else if( wp_get_post_parent_id($page) == self::get_id_of_second_tier_parent(self::get_currentID(),2 )){
+      return ' page-above-this-page ';
+    //Else remove because of how the Children are all called in multiple unnessary branches are dragged down
+    }else{
+      return ' spw-display-remove '; 
     }
   }
 
--- src/templates/_page_list.php
+++ src/templates/_page_list.php
@@ -7,9 +7,12 @@ use JB\SPW\Helpers;
 <div id="spw-page-list" class="spw-sidebar-object-div">
   <ul class="spw-sidebar-object-ul">
     <?php foreach ($pages as $page): ?>
-    <li class="spw-sidebar-object <?php echo Helpers::check_if_parent_page($page) ?>" >
-         <?php echo Helpers::build_sidebar_link($page) ?>
-    </li>
+      <?php if (Helpers::check_if_parent_is_active($page) != ' spw-display-remove '): ?>
+        <li class="spw-sidebar-object <?php echo Helpers::check_if_parent_page($page); echo Helpers::check_if_parent_is_active($page); ?>" >
+        
+            <?php echo Helpers::build_sidebar_link($page) ?>
+        </li>
+      <?php endif; ?>
     <?php endforeach; ?>
   </ul>
 </div>
